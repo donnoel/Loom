d29379948742e2712235fd7ab22544d72d20775f
donnoel <78627833+donnoel@users.noreply.github.com>
2026-01-27 12:14:53 -0800

    chore: initial commit (Loom project)

---
+++ LoomTests/LoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoomTests.swift
+//  LoomTests
+//
+//  Created by Don Noel on 1/27/26.
+//
+
+import Testing
+@testable import Loom
+
+struct LoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
